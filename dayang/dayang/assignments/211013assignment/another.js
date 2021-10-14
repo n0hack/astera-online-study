423af3101675d42af698b1ab3a31e13c5b5ff99e
LDH0531 <leedahan0531@gmail.com>
2021-10-14 20:39:25 +0900

    solve today's assignment (resolve #28)

---
+++ dayang/dayang/assignments/211013assignment/another.js
@@ -0,0 +1,14 @@
+
+function another(A,B,C){
+    let a = (A+B)%C
+    let b = ((A%C)+(B%C))%C
+    let c = (A*B)%C
+    let d = ((A%C)*(B%C))%C
+
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+}
+
+another(5,8,4)
---
+++ dayang/dayang/assignments/211013assignment/theCat.js
@@ -0,0 +1,27 @@
+
+let a = "\\";
+let b = "/";
+let c = "(";
+let d = ")";
+
+//kawaii cat
+console.log(`${a.padEnd(5)}${b}${a}`)
+console.log(`${d.padStart(2)}${c.padStart(3)} '${d}`)
+console.log(`${c.padEnd(3)}${b.padEnd(3)}${d}`)
+console.log(`${a.padStart(2)}${c}__${d}|`)
+
+
+
+/*
+1    67
+ 2  5 78
+1  4  7
+ 234567    
+
+
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+
+*/
